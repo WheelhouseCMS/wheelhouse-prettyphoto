488cc3bcde65b6e87988753e7dcb1491340cada1
Sam Pohlenz <sam@sampohlenz.com>
2011-12-01 11:31:53 +1030

    Fix javascript path and precompilation

--- app/helpers/pretty_photo/pretty_photo_helper.rb
+++ app/helpers/pretty_photo/pretty_photo_helper.rb
@@ -1,7 +1,7 @@
 module PrettyPhoto::PrettyPhotoHelper  
   def prettyphoto_assets
     plugin_assets {
-      stylesheet_link_tag("prettyphoto/prettyPhoto.css") + javascript_include_tag("prettyPhoto/jquery.prettyPhoto.js")
+      stylesheet_link_tag("prettyphoto/prettyPhoto.css") + javascript_include_tag("prettyphoto/jquery.prettyPhoto.js")
     }
   end
 end
--- lib/wheelhouse-prettyphoto.rb
+++ lib/wheelhouse-prettyphoto.rb
@@ -3,7 +3,7 @@ require 'wheelhouse'
 module PrettyPhoto
   class Plugin < Wheelhouse::Plugin
     initializer "precompile assets" do |app|
-      app.config.assets.precompile << "prettyPhoto/*"
+      app.config.assets.precompile << "prettyphoto/*"
     end
   end
 end
